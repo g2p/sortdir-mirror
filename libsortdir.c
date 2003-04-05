68d0b527d18b117f494437b8a82a63d383399512
T Ar Creator <tar@example.com>
2003-04-05 20:22:16 +0000

    Imported from archives/sortdir-0.3.tar.gz.

--- libsortdir.c
+++ libsortdir.c
@@ -1,22 +1,3 @@
-/*
-    libsortdir -- sort directory entries
-    Copyright (C) 2002, 2003  Egmont Koblinger <egmont@uhulinux.hu>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-*/
-
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <stdlib.h>
@@ -83,14 +64,14 @@ static int compare (const void *a, const void *b)
 	const struct dirent *a2 = a;
 	const struct dirent *b2 = b;
 	return do_compare(a2->d_name, b2->d_name);
-}
+} 
 
 static int compare64 (const void *a, const void *b)
 {
 	const struct dirent64 *a2 = a;
 	const struct dirent64 *b2 = b;
 	return do_compare(a2->d_name, b2->d_name);
-}
+} 
 
 struct dirent *readdir (DIR *dir)
 {
